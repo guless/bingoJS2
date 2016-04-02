84756cea5f14fc6e724df5173db7aea8e32b3772
winpzs <winpzs@gmail.com>
2016-04-02 11:31:56 +0800

    move file

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,21 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-var srcPath = 'd:/Projects/Iaas/PS/bingoJS2/scripts/bingo2/';
-var destPath = 'd:/Projects/Iaas/PS/bingoJS2/scripts/';
-var bingoPathMin = 'd:/Projects/Iaas/PS/bingoJS2/scripts/bingo.min.js';
+var srcPath = __dirname + '/src',
+    distPath = __dirname + '/dist',
+    bgSrcPath = srcPath + '/scripts/bingo2',
+    buildPath = distPath + "/scripts";
+
+console.log(__dirname);
+console.log(bgSrcPath);
+console.log(buildPath);
+
 var srcList = ['core.js', 'promise.js', 'event.js', 'observe.js',
     'package.js', 'linkNode.js', 'route.js', 'location.js', 'compiles.js',
     'service/base.js', 'command/base.js'];
 
 srcList = srcList.map(function (item) {
-    return srcPath + item;
+    return [bgSrcPath , item].join('/');
 });
 
 gulp.task('bingojs', ['concat', 'uglify']);
@@ -19,13 +25,13 @@ gulp.task('bingojs', ['concat', 'uglify']);
 gulp.task('concat', function () {
     return gulp.src(srcList)
            .pipe(concat('bingo.js'))
-          .pipe(gulp.dest(destPath));
+          .pipe(gulp.dest(buildPath));
 });
 
 gulp.task('uglify', function () {
     return gulp.src(srcList)
            .pipe(concat('bingo.min.js'))
             .pipe(uglify())
-          .pipe(gulp.dest(destPath));
+          .pipe(gulp.dest(buildPath));
 });
 
