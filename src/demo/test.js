bdd5cf06b0b05873b448ab731451382d9e13319b
winpzs <winpzs@gmail.com>
2016-04-23 17:00:29 +0800

    开始解释cp

--- src/demo/test.js
+++ src/demo/test.js
@@ -40,8 +40,18 @@
             app:null,
             cmd: '',
             attrs: null,
-            childCmd: null,
-            tranContext:null,
+            children: null,
+            getChild: function (id) {
+                var item;
+                //console.log(id, this.children);
+                bingo.each(this.children, function () {
+                    if (this.id == id) {
+                        item = this;
+                        return false;
+                    }
+                });
+                return item;
+            },
             extend: function (p) {
                 return bingo.extend(this, p);
             },
@@ -59,10 +69,10 @@
                 var ret = this._getContent();
                 if (_isPromise(ret))
                     ret.then(function (s) {
-                        this.childCmd = _traverseCmd(s, this.tranContext);
+                        return _traverseCmd(s, this);
                     }.bind(this));
                 else
-                    this.childCmd = _traverseCmd(ret, this.tranContext);
+                    ret = _traverseCmd(ret, this);
                 return ret;
             },
             layout: function (p, fn) {
@@ -81,7 +91,8 @@
             }
         }).extend(p);
     }, _newCPAttr = function (contents) {
-        return _newBindContext({ contents: contents
+        return _newBindContext({
+            contents: contents
         });
     };
 
@@ -126,48 +137,29 @@
         //解释else
         _checkElse = /\{\{\s*(\/?if|else)\s*(.*?)\}\}/gi,
         //解释指令属性: attr="fasdf"
-        _cmdAttrReg = /(\S+)\s*=\s*(?:\"((?:\\\"|[^"])*?)\"|\'((?:\\\'|[^'])*?)\')/gi,
-        _newTranContext = function () {
-            //解释上下文
-
-            return _newBase({
-                view:null,
-                _promises: [],
-                promise: function (p) {
-                    if (arguments.length > 0) {
-                        _promisePush(this._promises, p);
-                    } else {
-                        var prs = this._promises;
-                        this._promises = [];
-                        return _Promise.always(prs);
-                    }
-                }
-            });
-        };
+        _cmdAttrReg = /(\S+)\s*=\s*(?:\"((?:\\\"|[^"])*?)\"|\'((?:\\\'|[^'])*?)\')/gi;
 
     //scriptTag
     var _scriptTag = '<' + 'script type="text/html" bg-id="{0}"></' + 'script>',
         _getIdTag = function (id) { return _scriptTag.replace('{0}', id); };
 
-    var _traverseCmd = function (tmpl, tranContext) {
+    var _traverseCmd = function (tmpl, cp) {
         _commandReg.lastIndex = 0;
         var list = [], view, app;
         tmpl = tmpl.replace(_commandReg, function (find, cmd, attrs, cmd1, attrs1, content1) {
             //console.log('_commandEx', arguments);
             var id = bingo.makeAutoId(), elseList, item;
             if (cmd1 == 'if') {
-                var elseContent = _traverseElse(content1, tranContext);
+                var elseContent = _traverseElse(content1);
                 content1 = elseContent.contents;
                 elseList = elseContent.elseList;
             }
             item = {
                 id: id,
                 cmd: cmd1 || cmd,
-                tranContext: tranContext,
                 attrs: _traverseAttr(attrs1 || attrs),
                 contents: content1 || '',
-                elseList: elseList,
-                childCmd: null
+                elseList: elseList
             };
             (item.cmd == 'view') && (view = item);
             list.push(item);
@@ -175,22 +167,44 @@
             return _getIdTag(id);
         });
 
-        var cmdList = [], tmCP, cmdDef;
-        view && (tranContext.view = _newView({
-            name: view.attrs.getContens('name'),
-            app: bingo.app(view.attrs.getContens('app'))
-        }));
+        if (view) {
+            app = bingo.app(view.attrs.getContens('app'));
+            view = _newView({
+                name: view.attrs.getContens('name'),
+                app: app
+            });
+        } else {
+            app = cp.app;
+            view = cp.view;
+        }
+
+        var children = [], tempCP, cmdDef, elseList,
+                promises = [];
         bingo.each(list, function (item) {
-            tmCP = _newCP(item);
-            tmCP.view = tranContext.view;
-            tmCP.app = tmCP.view.app;
+            tempCP = _newCP(item);
+            tempCP.view = view;
+            tempCP.app = app;
             cmdDef = _defCommand(item.cmd);
-            cmdDef && cmdDef(tmCP);
-            tranContext.promise(tmCP.render());
-            cmdList.push(tmCP);
+            cmdDef && cmdDef(tempCP);
+            elseList = item.elseList;
+            if (elseList) {
+                var cpT
+                bingo.each(elseList, function (item, index) {
+                    cpT = _newCP({
+                        app: app,
+                        view: view, contents: item
+                    });
+                    _promisePush(promises, cpT.render());
+                    elseList[index] = cpT;
+                });
+            }
+            _promisePush(promises, tempCP.render());
+            children.push(tempCP);
         });
-        return { tmpl: tmpl, cmdList: cmdList };
-    }, _traverseElse = function (contents, tranContext) {
+        cp.children = children;
+        cp.tmplTag = tmpl;
+        return _retPromiseAll(promises);
+    }, _traverseElse = function (contents) {
         var lv = 0, item, cmd, index = -1, start = -1;
         _checkElse.lastIndex = 0;
         var elseList = [];
@@ -218,11 +232,6 @@
         }
         if (lv <= 0) {
             elseList.push(contents.substr(index));
-
-            bingo.each(elseList, function (item, index) {
-                elseList[index] = _traverseCmd(item, tranContext);
-            });
-
         }
 
         return { contents: start > -1 ? contents.substr(0, start) : contents, elseList: elseList };
@@ -239,12 +248,149 @@
         return attrs;
     };
 
-    //console.time('aaaa');
-    var _tranContext = _newTranContext(), cmdList = _traverseCmd(tmpl, _tranContext);
-    _tranContext.promise().then(function () {
-        console.log('cmdList', cmdList);
-        //console.timeEnd('aaaa');
+    var _doc = document,
+        _docEle = _doc.documentElement, _queryAll = function (selector, context) {
+            context || (context = _docEle);
+            return context.querySelectorAll(_qScope + selector);
+        }, _query = function (selector, context) {
+            context || (context = _docEle);
+            return context.querySelector(_qScope + selector);
+        };
+
+    /* 检测 scope */
+    var _qScope = ":scope ";
+    try {
+        _docEle.querySelector(":scope body");
+    } catch (e) {
+        _qScope = '';
+    }
+
+
+    var _removeNode = function (node) {
+        node.parentNode && node.parentNode.removeChild(node);
+    }, _injWithName = 'bingo_cmpwith_';
+
+    var _spTags = 'html,body,head', _wrapMap = {
+        select: [1, "<select multiple='multiple'>", "</select>"],
+        fieldset: [1, "<fieldset>", "</fieldset>"],
+        table: [1, "<table>", "</table>"],
+        tbody: [2, "<table><tbody>", "</tbody></table>"],
+        tr: [3, "<table><tbody><tr>", "</tr></tbody></table>"],
+        colgroup: [2, "<table><tbody></tbody><colgroup>", "</colgroup></table>"],
+        map: [1, "<map>", "</map>"],
+        div: [1, "<div>", "</div>"]
+    }, _scriptType = /\/(java|ecma)script/i,
+    _cleanScript = /^\s*<!(?:\[CDATA\[|\-\-)|[\]\-]{2}>\s*$/g, _globalEval = function (node) {
+        if (node.src) {
+            bingo.using(node.src);
+        } else {
+            var data = (node.text || node.textContent || node.innerHTML || "").replace(_cleanScript, "");
+            if (data) {
+                (window.execScript || function (data) {
+                    window["eval"].call(window, data);
+                })(data);
+            }
+        }
+    }, _parseSrcipt = function (container, script) {
+        bingo.each(container.querySelectorAll('script'), function (node) {
+            if (!node.type || _scriptType.test(node.type)) {
+                _removeNode(node);
+                script && _globalEval(node);
+            }
+        });
+    }, _parseHTML = function (html, p, script) {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="p">可以父节点或父节点tagName</param>
+        /// <param name="script">是否运行script</param>
+        /// <returns value=''></returns>
+        var tagName = p ? (bingo.isString(p) ? p : p.tagName.toLowerCase()) : '';
+        var wrap = _wrapMap[tagName] || _wrapMap.div, depth = wrap[0];
+        html = wrap[1] + html + wrap[2];
+        var container = _doc.createElement('div');
+        container.innerHTML = html;
+        while (depth--) {
+            container = container.lastChild;
+        }
+        var fr = document.createDocumentFragment();
+        bingo.each(container.childNodes, function (item) {
+            fr.appendChild(item);
+        });
+        _parseSrcipt(container, script);
+        return fr.childNodes;
+    }, _insertDom = function (nodes, refNode, fName) {
+        //fName:appendTo, insertBefore
+        bingo.each(nodes, function (item) {
+            if (item) {
+                if (fName == 'appendTo')
+                    refNode.appendChild(item);
+                else
+                    refNode.parentNode[fName](item, refNode);
+            }
+        });
+    };
+
+    var _traverseCP = function (node, tmpl, fName, cp) {
+
+        var nodes = _parseHTML(tmpl, node, true);
+        _traverseNodes(nodes, cp);
+        var fr = nodes.length > 0 ? nodes[0].parentNode : null
+        _insertDom([fr], node, fName);
+
+    }, _traverseNodes = function (nodes, cp) {
+
+        var id, tempCP;
+        bingo.each(nodes, function (item) {
+            if (item.nodeType == 1) {
+                id = _getBgTagId(item);
+                if (id) {
+                    tempCP = cp.getChild(id);
+                    if (tempCP) {
+                        console.log('tempCP', tempCP);
+                        _traverseCP(item, tempCP.tmplTag, 'insertBefore', tempCP);
+                        _removeNode(item);
+                    }
+                } else {
+                    _traverseNodes(_queryAll('script', item), cp);
+                }
+            }
+        });
+    }, _isScriptTag = /script/i, _getBgTagId = function (node) {
+        return (_isScriptTag.test(node.tagName)) ?
+            node.getAttribute('bg-id') : null;
+    };
+
+    var _compile = function (p) {
+        var view = p.view;
+        var cp = _newCP({
+            app:view ? view.app : null,
+            view: view, contents: p.tmpl
+        });
+        _Promise.resolve().then(function () { return cp.render(); }).then(function () {
+            console.log('compile', cp);
+            var node = _query('#context1');
+            var fr = _traverseCP(node, cp.tmplTag, 'appendTo', cp);
+            console.log('_traverseCP', node.innerHTML);
+        });
+    };
+
+    var _rootView = _newView({
+        name: 'rootView',
+        app: bingo.app('')
     });
+    _compile({
+        tmpl: tmpl,
+        view: _rootView
+    });
+
+    //console.time('aaaa');
+    //var _tranContext = _newTranContext(), cmdList = _traverseCmd(tmpl, _tranContext);
+    //_tranContext.promise().then(function () {
+    //    console.log('cmdList', cmdList);
+    //    //console.timeEnd('aaaa');
+    //});
 
 
     //查找dom 节点 <div>
