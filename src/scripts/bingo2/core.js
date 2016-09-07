de46f67770c5f5398fdc804c1c1ec009daffe89b
front-Y <9350003@qq.com>
2016-09-07 08:45:53 +0800

    优化代码

--- src/scripts/bingo2/core.js
+++ src/scripts/bingo2/core.js
@@ -20,7 +20,7 @@
 
     var bingo = window.bingo = {
         //主版本号.子版本号.修正版本号.编译版本号(日期)
-        version: { major: 2, minor: 1, rev: 'beta', build: 160821, toString: function () { return [this.major, this.minor, this.rev, this.build].join('.'); } },
+        version: { major: 2, minor: 1, rev: 0, build: 160907, toString: function () { return [this.major, this.minor, this.rev, this.build].join('.'); } },
         bgNoObserve: true,//防止observe
         isDebug: false,
         prdtVersion: '',
